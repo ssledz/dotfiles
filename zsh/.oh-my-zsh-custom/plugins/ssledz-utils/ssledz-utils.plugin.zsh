a5f865b865e23821ad10ec50b42bb210e53b0601
Sławomir Śledź <slawomir.sledz@gmail.com>
2016-07-17 00:47:52 +0200

    bzz

--- zsh/.oh-my-zsh-custom/plugins/ssledz-utils/ssledz-utils.plugin.zsh
+++ zsh/.oh-my-zsh-custom/plugins/ssledz-utils/ssledz-utils.plugin.zsh
@@ -4,6 +4,13 @@ for my_lib ($ZSH_CUSTOM/plugins/ssledz-utils/load-lib/*); do
   source $my_lib
 done
 
-alias cp1250-to-utf8='iconv -f cp1250 -t utf8'
-alias pmi='ssu_print_machine_info'
+alias conv-cp1250-to-utf8='iconv -f cp1250 -t utf8'
+alias print-machine-info='ssu_print_machine_info'
+alias print-motherboard-mem-slots='sudo dmidecode --type memory' #print memory slots on motherboard
 
+alias mount-cifs-ssledz='sudo mount -t cifs -o username=ssledz,iocharset=utf8,uid=ssledz'
+
+alias ds-mount-download='mount-cifs-ssledz //ds/downloads'
+alias ds-mount-download-mnt='ds-mount-download /mnt'
+alias ds-mount-home='mount-cifs-ssledz //ds/homes/ssledz'
+alias ds-mount-home-mnt='ds-mount-home /mnt'
