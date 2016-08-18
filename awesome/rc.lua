15ce052580992b48bc59a0de08106a4bf4cccb59
Sławomir Śledź <slawomir.sledz@gmail.com>
2016-08-18 17:53:37 +0200

    Client focus issue. Reference -> https://github.com/copycat-killer/awesome-copycats/issues/39

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -476,6 +476,14 @@ client.connect_signal("manage", function (c, startup)
     end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+--client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("focus",
+    function(c)
+        if c.maximized_horizontal == true and c.maximized_vertical == true then
+            c.border_color = beautiful.border_normal
+        else
+            c.border_color = beautiful.border_focus
+        end
+    end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
