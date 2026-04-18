e755b7e0343498d435911f3bbd6da5d9a7f48638
Sławomir Śledź <slawomir.sledz@dsstream.com>
2026-04-19 01:11:20 +0200

    Add configuration for opencode

--- restow.sh
+++ restow.sh
@@ -4,8 +4,10 @@ mkdir -p ~/.tmux/plugins
 mkdir -p ~/.urxvt
 mkdir -p ~/.config
 mkdir -p ~/.config/ranger
+mkdir -p ~/.config/opecode
 
 stow -R -v -t ~/ git
 #stow -R -v -t ~/ emacs
 stow -R -v -t ~/ tmux vim zsh
 stow -R -v -t ~/.config/ranger ranger
+stow -R -v -t ~/.config/opencode opencode
