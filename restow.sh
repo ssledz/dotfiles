6155b6cfbce017662132d7abdc64d63d2a54dcee
Sławomir Śledź <slawomir.sledz@dsstream.com>
2026-04-19 13:04:57 +0200

    Add spacemacs config

--- restow.sh
+++ restow.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 mkdir -p ~/.tmux/plugins
-mkdir -p ~/.urxvt
 mkdir -p ~/.config
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/opencode
 mkdir -p ~/.config/k9s
 
 stow -R -v -t ~/ git
-#stow -R -v -t ~/ emacs
 stow -R -v -t ~/ tmux vim zsh
 stow -R -v -t ~/.config/ranger ranger
 stow -R -v -t ~/.config/opencode opencode
 stow -R -v -t ~/.config/k9s k9s
+stow -R -v -t ~/ spacemacs
