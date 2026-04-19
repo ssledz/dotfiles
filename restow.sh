205c553cc3e8eb7f2bb1eeced47da3dfd04fd494
Sławomir Śledź <slawomir.sledz@dsstream.com>
2026-04-19 21:48:14 +0200

    Add configuration for 1password

--- restow.sh
+++ restow.sh
@@ -5,10 +5,12 @@ mkdir -p ~/.config
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/opencode
 mkdir -p ~/.config/k9s
+mkdir -p ~/.config/1Password/ssh/
 
 stow -R -v -t ~/ git
 stow -R -v -t ~/ tmux vim zsh
 stow -R -v -t ~/.config/ranger ranger
 stow -R -v -t ~/.config/opencode opencode
 stow -R -v -t ~/.config/k9s k9s
+stow -R -v -t ~/.config/1Password/ssh/ 1password
 stow -R -v -t ~/ spacemacs
